ee41178f614ea9e15d5a602eadd064aff47c9b00
JustAteAGrape <JustAteAGrape@users.noreply.github.com>
2016-02-28 21:30:37 -0500

    updating package file

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name = 'DSchaakCodeExercise',
-    version = '0.1',
-    description = 'AWeber Code Exercise from Daniel Schaak',
+    name = 'restarchive',
+    version = '1.0',
+    description = 'Code Exercise by Daniel Schaak',
     author= 'Daniel J. Schaak',
     author_email= 'dan.schaak@gmail.com',
-    url= ''
+    license='MIT',
+    url= 'https://github.com/JustAteAGrape/restarchive.git',
+    packages=['restarchive'],
+    zip_safe=False
 )
